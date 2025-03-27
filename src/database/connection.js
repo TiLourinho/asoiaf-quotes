a7822190bba144dfb26b8192af62cf7f2bb193a8
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-03-27 14:51:56 -0300

    Change PG connection from Client to Pool

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,23 @@
 import pg from "pg";
+import dotenv from "dotenv";
 
-const { Client } = pg;
-const db = new Client({
-  host: "localhost",
-  user: "postgres",
-  password: "56874646",
-  database: "asoiaf",
-  port: 5432,
+dotenv.config();
+
+const { Pool } = pg;
+const db = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DBPORT,
 });
 
-db.connect();
+try {
+  db.connect();
+  console.log("Server is connected to PostgreSQL");
+} catch (error) {
+  console.error("Server connection to PostgreSQL error", err.stack);
+  process.exit(1);
+}
 
 export default db;
