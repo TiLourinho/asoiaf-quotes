55b045c2b61b01af748e075067bb6cd3d4463310
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-03-27 17:15:51 -0300

    Refactor `quoteModel` file

---
+++ src/models/quoteModel.js
@@ -0,0 +1,88 @@
+import { StatusCodes } from "http-status-codes";
+
+import db from "../database/connection.js";
+import { errorHandler } from "../utils/tools.js";
+
+export async function getRandom() {
+  const query = `
+    SELECT * FROM quotes
+    ORDER BY random()
+    LIMIT 1;`;
+  const quote = (await db.query(query)).rows[0];
+
+  if (!quote) {
+    const message = "No quotes available";
+    throw errorHandler(StatusCodes.NOT_FOUND, message);
+  }
+
+  return quote;
+}
+
+export async function getById(id) {
+  const query = "SELECT * FROM quotes WHERE id = $1";
+  const quote = (await db.query(query, [id])).rows[0];
+
+  if (!quote) {
+    const message = "Quote not found";
+    throw errorHandler(StatusCodes.NOT_FOUND, message);
+  }
+
+  return quote;
+}
+
+export async function getRandomByCharacter(character) {
+  const query = `
+    SELECT * FROM quotes
+    WHERE character = $1
+    ORDER BY random()
+    LIMIT 1`;
+  const quote = (await db.query(query, [character])).rows[0];
+
+  if (!quote) {
+    const message = `No quotes found for ${character}`;
+    throw errorHandler(StatusCodes.NOT_FOUND, message);
+  }
+
+  return quote;
+}
+
+export async function create({ character, quote }) {
+  const query = `
+    INSERT INTO quotes (character, quote)
+    VALUES ($1, $2)
+    RETURNING *`;
+  const createdQuote = (await db.query(query, [character, quote])).rows[0];
+
+  if (!createdQuote) {
+    const message = "Failed to create quote";
+    throw errorHandler(StatusCodes.INTERNAL_SERVER_ERROR, message);
+  }
+
+  return createdQuote;
+}
+
+export async function update({ id, character, quote }) {
+  const query = `
+    UPDATE quotes SET character = $1, quote = $2
+    WHERE id = $3 RETURNING *`;
+  const updatedQuote = (await db.query(query, [character, quote, id])).rows[0];
+
+  if (!updatedQuote) {
+    const message = "Quote not found or update failed";
+    throw errorHandler(StatusCodes.NOT_FOUND, message);
+  }
+
+  return updatedQuote;
+}
+
+export async function remove(id) {
+  const query = `DELETE FROM quotes WHERE id = $1 RETURNING *`;
+  const removedQuote = (await db.query(query, [id])).rows[0];
+
+  if (!removedQuote) {
+    const message = "Quote not found or already removed";
+    throw errorHandler(StatusCodes.NOT_FOUND, message);
+  }
+
+  return removedQuote;
+}
--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,64 +1,8 @@
-import { readFileSync, writeFileSync } from "fs";
-
-import { QUOTES_PATH } from "../config/constants.js";
-
 export function serverStart(error, port) {
   if (error) console.log(`Error in server setup => ${error}`);
   console.log(`Server is up and listening on port ${port}`);
 }
 
-export function readQuotes() {
-  const quotes = readFileSync(QUOTES_PATH, "utf-8");
-  return JSON.parse(quotes);
-}
-
-export function getQuotes() {
-  const quotes = readQuotes();
-
-  if (!quotes || quotes.length === 0) {
-    throw new Error("No quotes available.");
-  }
-
-  return quotes;
-}
-
-export function getRandomNumber(limit) {
-  const randomNumber = Math.random() * limit.length;
-
-  return Math.floor(randomNumber);
-}
-
-export function saveQuotes(quote) {
-  const allQuotes = readQuotes();
-  const data = [...allQuotes, quote];
-  const stringfiedData = JSON.stringify(data, null, 2);
-
-  writeFileSync(QUOTES_PATH, stringfiedData);
-}
-
-export function updateQuotes(quote) {
-  const allQuotes = readQuotes();
-
-  allQuotes.map((item) => {
-    if (item.id === quote.id) {
-      item.id = item.id;
-      item.character = quote.character;
-      item.quote = quote.quote;
-    }
-  });
-
-  const data = [...allQuotes];
-  const stringfiedData = JSON.stringify(data, null, 2);
-
-  writeFileSync(QUOTES_PATH, stringfiedData);
-}
-
-export function removeQuotes(id) {
-  const allQuotes = readQuotes();
-  const filteredQuotes = allQuotes.filter((item) => item.id !== id);
-
-  const data = [...filteredQuotes];
-  const stringfiedData = JSON.stringify(data, null, 2);
-
-  writeFileSync(QUOTES_PATH, stringfiedData);
+export function errorHandler(status, message) {
+  return { status, message };
 }
